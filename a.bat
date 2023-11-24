eb6f1bc6ed365b8d8bd4b8d8d4c6a91ea8d9f3e0
Jaudakw <151969143+Jaudakw@users.noreply.github.com>
2023-11-24 21:07:06 +0000

    Create a.bat

---
+++ a.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+
+
+ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
+ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
+START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
+del /f /q "%SystemDrive%\diskpart.extend"
